696fb03360689f7e09878896457b1dcb1a7017dd
Tymani Ratchford <tratchfo@cs.brown.edu>
2017-04-16 12:46:54 -0400

    Fixed small typo/ need working help command

--- src/main/java/edu/brown/cs/soundpaint/Manager.java
+++ src/main/java/edu/brown/cs/soundpaint/Manager.java
@@ -21,16 +21,19 @@ public class Manager {
   }
 
   /**
-   * Returns the Pattern-Command mapping that defines the bacon
+   * Returns the Pattern-Command mapping that defines the soundpaint
    * program's CLI behavior.
    *
    * @return A map of all the Patterns and Commands that need to be recognized
    * by the CLI
    */
   public Map<Pattern, Command> getPatternCommandMap() {
-    return new ImmutableMap.Builder<Pattern, Command>()
-        .put(Pattern.compile("help"), this::helpCommand)
-        .build();
+	  ImmutableMap.Builder<Pattern, Command> toReturn = 
+			  new ImmutableMap.Builder<Pattern, Command>();
+	  toReturn.put(Pattern.compile("help"), this::helpCommand);
+	  toReturn.put(Pattern.compile("sound.*"),this::soundCommand);
+	  
+	  return toReturn.build();
   }
 
 
@@ -38,6 +41,10 @@ public class Manager {
     System.out.println("Welcome to SoundPaint's command line interface.");
     System.out.println("When commands are made available for use, they will be"
         + " listed here");
+  }
+  
+  public void soundCommand(List<String> tokens, String cmd) {
+	  System.out.println("sound command here!");
   }
 
   /**
