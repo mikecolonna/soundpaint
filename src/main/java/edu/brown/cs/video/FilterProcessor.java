f7f2c0da6083f3468b2f6298efdee2bf4a5e61ad
Michael Colonna <michaelcolonna@Michaels-MacBook-Pro-4.local>
2017-04-17 18:00:15 -0400

    Added 'filter' and 'process' commands. Filter chains can be created using the ffmpeg filter options (see documentation).

---
+++ src/main/java/edu/brown/cs/video/FilterProcessor.java
@@ -0,0 +1,70 @@
+package edu.brown.cs.video;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bytedeco.javacv.FFmpegFrameFilter;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FFmpegFrameRecorder;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameFilter;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.FrameRecorder;
+import org.bytedeco.javacv.FrameGrabber.Exception;
+
+public class FilterProcessor {
+
+  // internal filter graph
+  private FrameFilter filterer;
+  
+  public FilterProcessor(String filters) {
+    filterer = new FFmpegFrameFilter(filters, 0, 0);
+  }
+  
+  public void process(String inputPath, String outputPath) {
+    try {
+      FFmpegFrameGrabber videoGrabber 
+        = new FFmpegFrameGrabber(inputPath);
+      videoGrabber.start();
+      int imgWidth = videoGrabber.getImageWidth();
+      int imgHeight = videoGrabber.getImageHeight();
+      double frameRate = videoGrabber.getFrameRate();
+      int videoBitrate = videoGrabber.getVideoBitrate();
+      int videoCodec = videoGrabber.getVideoCodec();
+      
+      filterer.setImageWidth(imgWidth);
+      filterer.setImageHeight(imgHeight);
+      
+      FFmpegFrameRecorder recorder 
+        = new FFmpegFrameRecorder(outputPath, imgWidth, imgHeight);
+      recorder.setFormat("mp4");
+      recorder.setFrameRate(frameRate);
+      recorder.setVideoBitrate(videoBitrate);
+      recorder.setVideoCodec(videoCodec);
+      
+      filterer.start();
+      recorder.start();
+      
+      Frame curr = videoGrabber.grab();
+      while (curr != null) {
+        filterer.push(curr);
+        recorder.record(filterer.pull());
+        curr = videoGrabber.grab(); // get next frame
+      }
+      recorder.stop();
+      filterer.stop();
+      videoGrabber.stop();
+      
+      recorder.close();
+      videoGrabber.close();
+      
+      System.out.println("Video has finished processing.");
+    } catch (FrameGrabber.Exception fge) {
+      System.out.println("ERROR: Could not retrieve frame from video file.");
+    } catch (FrameFilter.Exception ffe) {
+      System.out.println("ERROR: Could not apply filter to frame.");
+    } catch (FrameRecorder.Exception fre) {
+      System.out.println("ERROR: Could not export frame to output.");
+    }
+  }
+}
