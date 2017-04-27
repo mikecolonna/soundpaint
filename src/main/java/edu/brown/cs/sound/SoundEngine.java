39f08718973aa5be94216a8c79cc783faf899040
Ty <Ty@Tys-MacBook-Pro.local>
2017-04-27 08:43:14 -0400

    further developed Sound Engine added javadoc and inline functions

--- src/main/java/edu/brown/cs/sound/SoundEngine.java
+++ src/main/java/edu/brown/cs/sound/SoundEngine.java
@@ -14,31 +14,46 @@ public class SoundEngine {
 
 	public void uploadSoundData(String filename, double id, double frameRate){
 		this.id = id;
+		
 		//get sound audio information
 		sr = new SoundRead(frameRate);
+		//all meta-data is set
 		sr.read(filename);
 	}
 	
 	public enum SoundParam {
-		AMPLITUDE,FREQUENCY,PITCH
+		AMPLITUDE,FREQUENCY,TEMPO
 	}
 	
 	public List<Double> getMetaData(SoundParam sp) {
 		List<Double> toReturn = new ArrayList<Double>();
 		switch(sp){
 		case AMPLITUDE:
+			toReturn = getAmpData();
 			break;
 		case FREQUENCY:
+			toReturn = getFreqData();
 			break;
-		case PITCH:
+		case TEMPO:
 			break;
 		}
 		return toReturn;
 		
 	}
 	
-	private List<Double> getFreqData(){
+	private List<Double> getFreqData() {
 		return sr.getFrequencyData();
+	}
+	
+	private List<Double> getAmpData() {
+		return sr.getAmplitudeData();
+	}
+	/**
+	 * Sends sound meta-data to database
+	 */
+	public void sendToDataBase() {
+		//TODO: Fill this function!
+		//remember to make make class that packages information in JSON
 	}
 	
 	
--- src/main/java/edu/brown/cs/sound/SoundRead.java
+++ src/main/java/edu/brown/cs/sound/SoundRead.java
@@ -4,39 +4,95 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import be.tarsos.dsp.pitch.AMDF;
 import be.tarsos.dsp.util.fft.FFT;
 import be.tarsos.dsp.util.fft.HammingWindow;
 
 public class SoundRead {
 	
+	//original sample rate of uploaded audio file
 	private double origSampleRate = 0;
-	private double timeResolution = (1.0/24.0);
+	
+	//the amount of seconds needed needed to be 
+	//sampled for video frame rate
+	private double timeResolution;
+	
+	//the amount of sound frames that 
+	//correspond to the timeResolution
 	private int numFramesTime = 0;
+	
+	//the total number of frames in the uploaded audio file
 	private int totalNumFrames = 0;
-	private List<SoundData> ampData = new ArrayList<SoundData>();
+	
+	//the tightest/smallest bound for the scaled 
+	//audio values to fit into
+	private double scaleMag = 1;
+	
+	//List of amplitudes from audio file in db (scaled)
+	private List<Double> ampData = new ArrayList<Double>();
+	
+	//List of frequency data from audio file in kilohertz(scaled)
 	private List<Double> freqData = new ArrayList<Double>();
 	
-	public SoundRead(double framRate) {
-		this.timeResolution = numFramesTime;
+	//List of tempoData from audio file in beats per minute (scaled)
+	private List<Double> tempoData = new ArrayList<Double>();
+	
+	
+	/**
+	 * SoundRead is a class that calculates the 
+	 * meta-data(frequency,tempo, and amplitude) from audio files 
+	 * @param frameRate - video frame sample rate for audio data to be sampled
+	 */
+	public SoundRead(double frameRate) {
+		this.timeResolution = frameRate;
 	}
 	
-	public void read(String filename){
+	
+	/**
+	 * Takes in audio filepaths for mp3 (coming soon), wav, and midi
+	 * files and uploads their data
+	 * @param filename - filepath for audio file
+	 */
+	public void read(String filename) {
+		
+		//splitting input to find file type
 		String [] splitInput = filename.split("/");
 	    String fileName = splitInput[splitInput.length - 1];
 	     
-	    if(fileName.contains(".wav")){
+	    if(fileName.contains(".wav")) {
 	    	  readWav(filename);
-	    }else if(fileName.contains(".mid")){
+	    }else if(fileName.contains(".mid")) {
 	    	  readMidi(filename);
+	    }else if(fileName.contains(".mp3")) {
+	    		readMp(filename); 
 	    }else{
 	    	System.out.println("ERROR: File is not a midi or wave file");
 	    }
 	}
-	
+	//TODO: Abstract read functions into classes, 
+	//and make an interface for frequency and tempo collection
+	/**
+	 * Reads audio file and call functions to 
+	 * collect meta-data
+	 * @param filename - audio file path
+	 */
+	private void readMp(String filename) {
+		
+	}
+	/**
+	 * Reads audio file and call functions to 
+	 * collect meta-data
+	 * @param filename - audio file path
+	 */
 	private void readMidi(String filename){
 		
 	}
 	
+	/**
+	 * Reads audio file and call functions to 
+	 * collect meta-data
+	 * @param filename - audio file path
+	 */
 	private void readWav(String filename){
 		try
 	      {
@@ -58,8 +114,7 @@ public class SoundRead {
 	         
 	         double sampleRate = wavFile.getSampleRate();
 	         origSampleRate = sampleRate;
-	         double maxTime = 0;
-	         double minTime = 0;
+	         
 	         int count = 0;
 	         
 	         do
@@ -72,104 +127,179 @@ public class SoundRead {
 	          
 	            for (int s=0 ; s<framesRead * numChannels ; s++)
 	            {
+	            	//checks to find amount of audio frames that correlate to 
+	            	//amount of video frames
 	            	if(count/sampleRate <= timeResolution+(1/sampleRate) && 
 	            			count/sampleRate > timeResolution) {
-	            		System.out.println(count/sampleRate > timeResolution);
 	            		numFramesTime = count;
 	            	}
-	            	ampData.add(new SoundData(count/sampleRate,new Amplitude(buffer[s])));
+	            	
+	            	//add amplitude data
+	            	ampData.add((buffer[s]));
 	            	
 	            	
 	               if (buffer[s] > max) {
 	            	   max = buffer[s];
-	            	   maxTime = count/sampleRate;
 	            	
 	               }
 	               if (buffer[s] < min) {
 	            	   min = buffer[s];
-	            	   minTime = count/sampleRate;
 	               }
 	               count++;
 	            }
 	         }
 	         while (framesRead != 0);
 	         
+	         //set total amount of frame sin audio file
 	         totalNumFrames = count;
-	        System.out.println("TOTAL LENGTH SECS: " + count/sampleRate);
 	        
 	         // Close the wavFile
 	         wavFile.close();
-
-	         // Output the minimum and maximum value
-	         System.out.printf("Min: %f, Max: %f\n", min, max);
-	         System.out.printf("MinTime: %f, MaxTime: %f\n", minTime, maxTime);
 	         
+	         //scale amplitude data
+	         ampData = scaleData(max,min,ampData);
+	         
+	         //get rest of meta data
+	         getFrequencyData();
 	      }
 	      catch (Exception e)
 	      {
 	         System.err.println(e);
 	      }
 	}
-	//or maybe do time as input
-	//if num samples is greater thna amount that can be given then 
-	//overlap is given in a percentage amount (i.e .3 -> 30%)
+	
+	
+	/**
+	 * Returns amplitude data 
+	 * @return
+	 */
+	public List<Double> getAmplitudeData() {
+		return ampData;
+	}
+	/**
+	 * Returns tempo data
+	 * @return
+	 */
+	public List<Double> getTempoData() {
+		return tempoData;
+	}
+	/**
+	 * Collects and sets frequency and tempo data
+	 * @return
+	 */
 	public List<Double> getFrequencyData() {
+		//check if frequency Data is cached 
+		if(freqData.isEmpty()) {
+			
+			//define overlap of frame sound samples
 			double overlap = 0.5;
+			
+			//List of video frame chunks/ frames that have 
+			// been transformed by fft
 			List<float []> fftData = new ArrayList<float []>();
+			
+			//counter to find the beginning of every video frame sample
 			int lastAdd = 0;
-			System.out.println("LAST ADD: " + lastAdd +
-					"numFramesTime: " + numFramesTime +
-					"total num frames: " + totalNumFrames);
+			
+			//loops through every chunk of video frame data from sound data
 			while(lastAdd+(numFramesTime-1) < totalNumFrames){
+					
+					//define video frame rate chunk 
 					float [] toAdd = new float [numFramesTime];
+					
+					//add sound data into video frame rate chunk
 					for(int i = 0; i < numFramesTime;i++) {
-						toAdd[i] = (float)ampData.get(lastAdd + i).getValue();
+						toAdd[i] = ampData.get(lastAdd + i).floatValue();
 					}
+					
+					//add video chunk to fft samples list
 					fftData.add(toAdd);
+					
+					//find next start of video frame chunk
 					lastAdd = (int) Math.ceil(lastAdd+(numFramesTime-1)*overlap) - 1;
 				
 				
 			}
-			System.out.println("Done!sound");
+			
+			//List of FFT Objects indexed according to fftData
 			List<FFT> fftList = new ArrayList<FFT>();
-			List<double []> allFreq = new ArrayList<double []>();
+		
+			//loop through each video chunk to transform via fft the 
+			//sound data in each chunk
 			for(float [] f: fftData) {
+				
+				//Define FFT object
 				FFT transform = new FFT(f.length,new HammingWindow());
+				
+				//FFT forward transform on f in fftData
 				transform.forwardTransform(f);
-				double [] freqSample = new double [f.length/2];
-				for(int i = 0; i < freqSample.length; i++){
-					freqSample[i] = transform.binToHz(i, 44100);
-				}
-				allFreq.add(freqSample);
+				
+				//add FFT object to list
 				fftList.add(transform);
 			}
 			
 		
 			
-			System.out.println("Number of bins (should be): " + numFramesTime/2);
-			System.out.println("What it is: " + fftData.get(0).length);
+//			System.out.println("Number of bins (should be): " + numFramesTime/2);
+//			System.out.println("What it is: " + fftData.get(0).length);
+//			
+//			System.out.println("BIN VALUE FOR LAST BIN: " + fftList.get(0).binToHz(fftData.get(0).length -1, 44100));
+//			System.out.println("BIN VALUE FOR HALF BIN: " + fftList.get(0).binToHz(fftData.get(0).length/2, 44100));
+//			System.out.println("BIN VALUE FOR first  BIN: " + fftList.get(0).binToHz(1, 44100));
 			
-			System.out.println("BIN VALUE FOR LAST BIN: " + fftList.get(0).binToHz(fftData.get(0).length -1, 44100));
-			System.out.println("BIN VALUE FOR HALF BIN: " + fftList.get(0).binToHz(fftData.get(0).length/2, 44100));
-			System.out.println("BIN VALUE FOR first  BIN: " + fftList.get(0).binToHz(1, 44100));
 			
-			for(int i = 0; i < fftData.size();i++){
-				double toAdd = fftList.get(i).binToHz(findMax(fftData.get(i)), 
+			//loop through fft samples
+			for(int i = 0; i < fftData.size();i++) {
+				
+				//freq is in kilohertz
+				double toAdd = fftList.get(i).binToHz(findMaxIndex(fftData.get(i)), 
 						(float)origSampleRate);
+				
+				//tempo defined as beats per minute
+				//1 khz is equal to 60000 beats per minute
+				tempoData.add(toAdd*60000);
+				
 				freqData.add(toAdd);
 			}
-		
 			
+			double maxValue = findMaxValue(freqData);
+			double minValue = findMinValue(freqData);
 			
+			//final tempoData scaled
+			tempoData = scaleData(maxValue*60000,minValue*60000,tempoData);
 			
-			
-			
-			
-			return freqData;	
+			//final frequency scaled
+			freqData = scaleData(maxValue,minValue,freqData);
+	
+		}
+		return freqData;
+	}
+	/**
+	 * Scales sound data within pre-
+	 * determined scaleMag
+	 * @param max - max sound data value
+	 * @param min - min sound data value
+	 * @param toScale - list of data to be scaled
+	 * @return scaled data
+	 */
+	private List<Double> scaleData(double max, double min, List<Double> toScale) {
+		double biggestMag = -1;
+		if(Math.abs(max) < Math.abs(min)) {
+			biggestMag = min;
+		} else {
+			biggestMag = max;
+		}
+		double scaleFactor = Math.abs(scaleMag)/biggestMag;
+		List<Double> toReturn = new ArrayList<Double>();
+		for(double d: toScale){
+			toReturn.add(d*scaleFactor);
+			System.out.println(d*scaleFactor);
+		}
+		return toReturn;
 		
 	}
 	
-	private int findMax(float[] data){
+	private int findMaxIndex(float[] data){
 		float max = Float.MIN_VALUE;
 		int toReturn = -1;
 		//only look through half of data because of nyquist
@@ -181,6 +311,28 @@ public class SoundRead {
 		}
 		
 		return toReturn;
+	}
+	
+	private double findMaxValue(List<Double> lst) {
+		Double max = Double.MIN_VALUE;
+		for(double d: lst) {
+			if(d > max) {
+				max = d;
+			}
+		}
+		
+		return max;
+	}
+	
+	private double findMinValue(List<Double> lst) {
+		Double min = Double.MAX_VALUE;
+		for(double d: lst) {
+			if(d < min) {
+				min = d;
+			}
+		}
+		
+		return min;
 	}
 	
 	
