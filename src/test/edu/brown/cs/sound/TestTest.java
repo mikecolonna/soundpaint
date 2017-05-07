54e1f8af5034c33782cb69a32e875ae02b7768ba
Tynan Couture-Rashid <tynancr@gmail.com>
2017-05-07 14:23:20 -0400

    Added sample unit test.

---
+++ src/test/edu/brown/cs/sound/TestTest.java
@@ -0,0 +1,14 @@
+package edu.brown.cs.sound;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by Tynan on 5/7/17.
+ */
+public class TestTest {
+  @Test
+  public void testTest() {
+    assertTrue(true);
+  }
+}
