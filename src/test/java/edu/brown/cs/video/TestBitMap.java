063bc20a1c100b491f25e1d269d452dc41da0a51
Ty <Ty@Tys-MacBook-Pro.local>
2017-05-09 08:19:36 -0400

    Even more testing

---
+++ src/test/java/edu/brown/cs/video/TestBitMap.java
@@ -0,0 +1,17 @@
+package edu.brown.cs.video;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ty on 09/05/2017.
+ */
+public class TestBitMap {
+
+  @Test
+  public void testInit() {
+    BitmapSequence b = new BitmapSequence();
+    assertNotNull(b);
+
+  }
+}
