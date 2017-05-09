fe943923674e299306b955e8460b6c609d1f7aff
Ty <Ty@Tys-MacBook-Pro.local>
2017-05-09 08:27:43 -0400

    Even more tests testzzzzz

---
+++ src/test/java/edu/brown/cs/video/TestFilterProcessor.java
@@ -0,0 +1,16 @@
+package edu.brown.cs.video;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ty on 09/05/2017.
+ */
+public class TestFilterProcessor {
+
+  @Test
+  public void testInit() {
+    FilterProcessor fp = new FilterProcessor("test_filter");
+    assertNotNull(fp);
+  }
+}
